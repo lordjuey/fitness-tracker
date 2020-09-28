a97b3f44ef81a81d0cb5f71409e944f58a3211be
Juwon <lordjuey@gmail.com>
2020-09-29 01:14:00 +1000

    server.js issues

--- server.js
+++ server.js
@@ -1,25 +1,30 @@
 const express = require("express");
-const logger = require("morgan");
+const morgan = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
-const User = require("./userModel.js");
-
 const app = express();
 
-app.use(logger("dev"));
-app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnesstracker", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
 
-//routes
-require("./routes/apiroutes")(app);
-require("./routes/htmlRoutes")(app);
+
+require("./routes/api_routes")(app);
+
+require("./routes/html_Routes")(app);
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`App running on port ${PORT}!:D`);
 });
